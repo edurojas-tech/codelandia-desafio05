07d9d6ab30da229c0110fbb12ff22df75ede70c6
Eduardo Rojas <edurojas.developer@gmail.com>
2023-05-25 23:08:16 -0300

    refactor(navbar/links): criando schema para iterar dados de links e reaproveitando tag de link <a>

--- app/src/navbar/component.jsx
+++ app/src/navbar/component.jsx
@@ -1,24 +1,20 @@
-import './style.css'
-import logo from '../assets/logo.svg'
-import facebookIcon from '../assets/facebook.svg'
-import instaIcon from '../assets/instagram.svg'
-import youtubeIcon from '../assets/youtube.svg'
-import twitter from '../assets/twitter.svg'
+import "./style.css";
+import logo from "../assets/logo.svg";
+import Links from "./schema";
 
 function component() {
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="navbar__logo">
-        <img src={logo} alt="logo" className='navbar__imagem' />
+        <img src={logo} alt="logo" className="navbar__imagem" />
       </div>
       <div className="navbar__links">
-        <a href=""><img src={facebookIcon} alt="" /></a>
-        <a href=""><img src={instaIcon} alt="" /></a>
-        <a href=""><img src={youtubeIcon} alt="" /></a>
-        <a href=""><img src={twitter} alt="" /></a>
+        {Links.map(link => (
+          <a href={link.desc} target="_blank"><img src={link.icon} alt="" /></a>
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default component
+export default component;
---
+++ app/src/navbar/schema.js
@@ -0,0 +1,23 @@
+import facebookIcon from '../assets/facebook.svg'
+import instaIcon from '../assets/instagram.svg'
+import youtubeIcon from '../assets/youtube.svg'
+import twitter from '../assets/twitter.svg'
+const links = [
+  {
+    desc: "https://www.facebook.com",
+    icon: facebookIcon
+  },
+  {
+    desc: "https://www.instagram.com/edurojas.dev",
+    icon: instaIcon
+  },{
+    desc: "https://youtube.com",
+    icon: youtubeIcon
+  },
+  {
+    desc: "https://twitter.com",
+    icon: twitter
+  }
+]
+
+export default links
